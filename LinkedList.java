6a8f0080c2e57f78d9454fe5b794170b1b8b8df3
7506574455 <84278635+7506574455@users.noreply.github.com>
2021-06-30 10:44:30 -0700

    Added: linked list push type code

--- LinkedList.java
+++ LinkedList.java
@@ -1,46 +1,56 @@
 package com.ds;
 
-public class LinkedList {
 
+public class LinkedList {
+	//creating head node 
 	Node head;
-	
-	class Node {
-		
-		int data;
+
+	class Node{
 		Node next;
-		
+		int data;
+
+		//constructor 
 		public Node(int data) {
+			super();
+			this.next = next;
 			this.data = data;
-			this.next = null;
-		}
-		
-	}
-	
-	public void insert(int data) {
-	
-		Node newNode = new Node(data);
-		if(head == null) {
-		    head = newNode;
-		}
-		else {
-		    Node temp = head;
-		    while(temp.next != null) {
-			    temp = temp.next;
-		    }
-		
-		    temp.next = newNode;
 		}
 	}
-	
+	//method to insert data in LinkList
+	public Node insert(int data) {
+		 Node newNode = new Node(data);
+		 if (head == null)
+		 {
+			 head = newNode;
+		 }
+		 else
+		 {
+			 Node temp = head;
+			 while (temp.next != null)
+			 {
+			 temp = temp.next;
+			 }
+			 temp.next = newNode;
+		 }
+		 return newNode;
+	 }
+
+	//method to push(Insertion at head node)
+	public void push(int data) {
+		 Node newNode = new Node(data);
+		 newNode.next = head;
+		 head = newNode;
+	 }
+
+	//method to Display LinkList
 	public void print() {
-		Node temp = head;
-		while(temp != null) {
-			System.out.print(temp.data+ " -> ");
-			temp = temp.next;
-		}
-		System.out.println();
-	
-		
-	}
-	
+		 Node temp = head;
+		 while (temp != null)
+		 {
+			 System.out.print(temp.data + " -> ");
+			 temp = temp.next;
+		 }
+		 System.out.println();
+	 }
+
 }
